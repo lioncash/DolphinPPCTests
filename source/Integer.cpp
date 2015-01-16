e72318e53cb95e9ed3400269292c6aac8d5b31e6
Lioncash <mathew1800@gmail.com>
2015-01-16 13:18:26 -0500

    Integer: Fix opcode test formatting

--- source/Integer.cpp
+++ source/Integer.cpp
@@ -5,31 +5,6 @@
 #include "tests.h"
 #include "Utils.h"
 
-// Test for a 2-component instruction
-// e.g. ADDME rD, rA
-#define OPTEST_2_COMPONENTS(inst, rA)                                       \
-{                                                                           \
-    u32 output;                                                             \
-    u32 ra = rA;                                                            \
-                                                                            \
-    asm volatile (inst " %[out], %[Ra]": [out]"=&r"(output) : [Ra]"r"(ra)); \
-                                                                            \
-    printf("%s   :: rD 0x%08X | rA 0x%08X", inst, output, rA);              \
-}
-
-// Test for a 3-component instruction
-// e.g. ADD rD, rA, rB
-#define OPTEST_3_COMPONENTS(inst, rA, rB)                                                       \
-{                                                                                               \
-    u32 output;                                                                                 \
-    u32 ra = rA;                                                                                \
-    u32 rb = rB;                                                                                \
-                                                                                                \
-    asm volatile (inst " %[out], %[Ra], %[Rb]": [out]"=&r"(output) : [Ra]"r"(ra), [Rb]"r"(rb)); \
-                                                                                                \
-    printf("%s     :: rD 0x%08X | rA 0x%08X | rB 0x%08X\n", inst, output, rA, rB);              \
-}
-
 static u32 GetXER()
 {
     u32 xer;
@@ -44,12 +19,42 @@ static u32 GetCR()
     return cr;
 }
 
+// Test for a 2-component instruction
+// e.g. ADDME rD, rA
+#define OPTEST_2_COMPONENTS(inst, rA)                                                    \
+{                                                                                        \
+    u32 output;                                                                          \
+    u32 ra = rA;                                                                         \
+                                                                                         \
+    asm volatile (inst " %[out], %[Ra]": [out]"=&r"(output) : [Ra]"r"(ra));              \
+                                                                                         \
+    printf("%s   :: rD 0x%08X | rA 0x%08X | XER: 0x%08X\n", inst, output, rA, GetXER()); \
+}
+
+// Test for a 3-component instruction
+// e.g. ADD rD, rA, rB
+#define OPTEST_3_COMPONENTS(inst, rA, rB)                                                                \
+{                                                                                                        \
+    u32 output;                                                                                          \
+    u32 ra = rA;                                                                                         \
+    u32 rb = rB;                                                                                         \
+                                                                                                         \
+    asm volatile (inst " %[out], %[Ra], %[Rb]": [out]"=&r"(output) : [Ra]"r"(ra), [Rb]"r"(rb));          \
+                                                                                                         \
+    printf("%-8s :: rD 0x%08X | rA 0x%08X | rB 0x%08X | XER: 0x%08X\n", inst, output, rA, rB, GetXER()); \
+}
+
 void PPCIntegerTests()
 {
-    OPTEST_3_COMPONENTS("ADD", 0, 1);
+    OPTEST_3_COMPONENTS("ADD", 0xFFFFFFFF, 1);
     OPTEST_3_COMPONENTS("ADD", -1, 1);
     OPTEST_3_COMPONENTS("ADD", -1, -1);
-    OPTEST_3_COMPONENTS("AND", 1, 0);
-    OPTEST_3_COMPONENTS("AND", 1, 1);
-    OPTEST_3_COMPONENTS("AND", 420, 69);
+    OPTEST_3_COMPONENTS("ADD", 1, 0);
+    OPTEST_3_COMPONENTS("ADD", 1, 1);
+    OPTEST_3_COMPONENTS("ADDE", 0xFFFFFFFF, 1);
+    OPTEST_3_COMPONENTS("ADDE", -1, 1);
+    OPTEST_3_COMPONENTS("ADDE", -1, -1);
+    OPTEST_3_COMPONENTS("ADDE", 1, 0);
+    OPTEST_3_COMPONENTS("ADDE", 1, 1);
+    
 }
