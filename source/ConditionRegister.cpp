4fd8243f7573b6acf8392597bd81a6732c4f69f9
Lioncash <mathew1800@gmail.com>
2015-12-09 13:54:42 -0500

    ConditionRegister: Remove inline specifier

--- source/ConditionRegister.cpp
+++ source/ConditionRegister.cpp
@@ -23,7 +23,7 @@
 
 // Note: This'll generate wrong values on little-endian PPC.
 //       So, uhh... don't use this on little-endian.
-static inline uint32_t GetShiftValue(uint32_t mask)
+static uint32_t GetShiftValue(uint32_t mask)
 {
     switch (mask)
     {
