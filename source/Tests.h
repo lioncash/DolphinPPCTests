99b7a82c7e7a6584e418dc95e80873525e353cd5
Lioncash <mathew1800@gmail.com>
2015-12-09 13:22:30 -0500

    Tests: Remove static specifier from header functions

--- source/Tests.h
+++ source/Tests.h
@@ -2,26 +2,26 @@
 
 #include <cstdint>
 
-static inline uint32_t GetCR()
+inline uint32_t GetCR()
 {
     uint32_t reg;
     asm volatile ("mfcr %[out]" : [out]"=&r"(reg));
     return reg;
 }
 
-static inline void SetCR(uint32_t value)
+inline void SetCR(uint32_t value)
 {
     asm volatile ("mtcr %[val]" : : [val]"r"(value) : "cr0", "cr1", "cr2", "cr3", "cr4", "cr5", "cr6", "cr7");
 }
 
-static inline uint32_t GetXER()
+inline uint32_t GetXER()
 {
     uint32_t xer;
     asm volatile ("mfxer %[out]" : [out]"=&r"(xer));
     return xer;
 }
 
-static inline void SetXER(uint32_t value)
+inline void SetXER(uint32_t value)
 {
     asm volatile ("mtxer %[val]" : : [val]"r"(value) : "xer");
 }
